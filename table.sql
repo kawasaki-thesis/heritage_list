7c307fe4e02ac19e4ae1d992fdbb1c9e29f40c04
kawasaki-thesis <t14253sk@sfc.keio.ac.jp>
2017-12-28 21:16:31 +0900

    テーブル部分のSQLを作成。INSERT部分は自動生成なので分ける

---
+++ table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS world_heritage;
+CREATE TABLE world_heritage(
+  name text,
+  country text,
+  area text,
+  longitude numeric,
+  latitude numeric,
+  c1 integer,
+  c2 integer,
+  c3 integer,
+  c4 integer,
+  c5 integer,
+  c6 integer,
+  c7 integer,
+  c8 integer,
+  c9 integer,
+  c10 integer
+);
